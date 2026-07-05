a6d7b6a4527bed38b985702a80e9d1ee3e32c602
VojinPeric <pv220147d@student.etf.bg.ac.rs>
2026-07-05 15:32:49 +0200

    Changed semantics of pgp_messages

--- src/pgp_messages.py
+++ src/pgp_messages.py
@@ -1,41 +1,33 @@
 """
-File containing transition message types between pgp protocol steps
+File containing transition message types between pgp protocol steps.
+Since some steps in PGP can be skipped all steps (signature, encryption, etc.) 
+should just send array of bytes forward.
 """
 
+from datetime import datetime, timezone
+
 class Message:
-    __slots__ = ("header", "body")
+    __slots__ = ("msg", "filename", "timestamp")
 
     def __init__(self):
-        self.header = ""
-        self.body = ""
+        self.msg = ""
+        self.filename = ""
+        self.timestamp = datetime.now(timezone.utc)
 
 class SignedMessage:
-    __slots__ = ("rawMessage", "signature")
+    __slots__ = ("rawMessage", "signature", "leadingTwoOctets", "keyId", "timestamp")
 
     def __init__(self):
         self.rawMessage = bytes()
         self.signature = bytes()
-
-class CompressedMessage:
-    __slots__ = ("msg")
-
-    def __init__(self):
-        self.msg = bytes()
+        self.leadingTwoOctets = bytes()
+        self.keyId = bytes()
+        self.timestamp = datetime.now(timezone.utc)
 
 class EncryptedMessage:
-    __slots__ = ("msg")
+    __slots__ = ("msg", "encryptedSessionKey", "keyId")
 
     def __init__(self):
         self.msg = bytes()
-
-class EncodedMessage:
-    __slots__ = ("msg")
-
-    def __init__(self):
-        self.msg = ""
-
-class SegmentatedMessage:
-    __slots__ = ("parts")
-
-    def __init__(self):
-        self.parts = []
+        self.encryptedSessionKey = bytes()
+        self.keyId = bytes()
